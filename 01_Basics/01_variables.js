5987db6e5c8c1bb2199f772a5a1ad75de4ad5693
agastya-prince <113304483+agastya-prince@users.noreply.github.com>
2024-06-21 01:17:12 +0000

    basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "agastya@email.com"
+var accountPassword = "1234"
+accountCity = "RPH"
+let accountState
+
+// accountId = 2 // constants cannot ever change
+console.log(accountId)
+
+accountEmail = "prince@email.com"
+accountPassword = "5678"
+accountCity = "DMK"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// let, var, change. 
+//Use let as var doesn't have scope of use. it changes varnames even outside scopes
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // this treats all JS Code as newer version
+// newer version has classes, modules, etc
+
+// alert(3+3) // will not give 6 as we are using nodejs, not browser
+
+// code readibility is paramount
+
+let name = "Prince"
+let age = 21
+let isLoggedIn = true
+
+// data types are 
+// number => 2 to power 53
+// bigInt for greater numbers
+// string
+// boolean => true/false
+// null => standalone value , means empty, no value
+// undefined, means value not assigned
+// symbol => unique
+
+
+// object
+console.log(typeof "xyz") // returns data type of var or const
+console.log(typeof null)  // object 
+console.log(typeof undefined)  // typeof undefined
